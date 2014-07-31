ad10571c19c426bdda1a6b89aa7146efd0c28619
MikiWakana <o-ce@o-ce.local>
2014-07-31 20:34:22 +0900

    v.p2
    main

---
+++ foot.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<meta charset="UTF-8">
+<meta name="Author" content="codeCrystalgeysers">
+<title>HAL Cinema</title>
+<footer>
+    <nav id="footMenu">
+        <div class="footMenuBox">
+            <section>
+                <h2><a href="index.php">HALCinema</a></h2>
+                <ul>
+                    <li><a href="index.php">HOME</a></li>
+                    <li><a href="news.php">お知らせ</a></li>
+                    <li><a href="#">プライバシーポリシー</a></li>
+                    <li><a href="#">特定商取引法に基づく表記</a></li>
+                    <li><a href="faq.php">よくある質問</a></li>
+                    <li><a href="mail.php">お問い合わせ</a></li>
+                </ul>
+            </section>
+        </div>
+        <div class="footMenuBox">
+            <section>
+                <h2><a href="article.php">映画情報</a></h2>
+                <ul>
+                    <li><a href="schedule.php">上映スケジュール</a></li>
+                    <li><a href="feature.php">作品情報</a></li>
+                </ul>
+            </section>
+            <section>
+                <h2><a href="theater.php">館内情報</a></h2>
+                <ul>
+                    <li><a href="theater.php">施設案内</a></li>
+                    <li><a href="theater.php">上映設備</a></li>
+                </ul>
+            </section>
+        </div>
+        <div class="footMenuBox">
+            <section>
+                <h2><a href="price.php">料金</a></h2>
+                <ul>
+                    <li><a href="price.php">チケット料金</a></li>
+                    <li><a href="price.php">グッズ</a></li>
+                    <li><a href="price.php">フード</a></li>
+                </ul>
+            </section>
+        </div>
+        <div class="footMenuBox">
+            <section>
+                <h2><a href="theater.php">アクセス</a></h2>
+                <ul>
+                    <li><a href="theater.php">施設案内</a></li>
+                    <li><a href="#">HALCinema OSAKA</a></li>
+                    <li><a href="#">HALCinema NAGOYA</a></li>
+                </ul>
+            </section>
+        </div>
+        <div class="footMenuBox">
+            <section>
+                <ul>
+                    <li><a href="#">スマートフォンサイト</a></li>
+                    <li><a href="#">Facebook</a></li>
+                    <li><a href="#">Twitter</a></li>
+                </ul>
+            </section>
+        </div>
+    </nav>
+    <section id="copyright"><p>Copyright &copy;codeCrstalgeysers All rights reserved.</p></section>
+</footer>
---
+++ head.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<meta charset="UTF-8">
+<meta name="Author" content="codeCrystalgeysers">
+<title>HAL Cinema</title>
+
+<header>
+    <h1 id="logo">HALCinema</h1>
+    <div id="alert">
+        ※アラート予約
+    </div>
+    <div id="login">
+        <button id="btnLogin">ログイン</button>
+        <div id="nowLogin">
+            <p>IDまたは名前表示</p>
+            <button id="btnLogout">ログアウト</button>
+        </div>
+    </div>
+    <nav id="headMenu">
+        <ul>
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="schedule.php">上映スケジュール</a></li>
+            <li><a href="article.php">特集</a></li>
+            <li><a href="theater.php">館内情報</a></li>
+            <li><a href="price.php">料金</a></li>
+            <li><a href="access.php">アクセス</a></li>
+        </ul>
+    </nav>
+</header>
