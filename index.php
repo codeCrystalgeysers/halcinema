61f98beeb5cca339414a8bcc52ba42cc79d63432
MikiWakana <o-ce@o-ce.local>
2014-07-31 22:35:45 +0900

    v.p4
    v.3

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<meta charset="UTF-8">
+<meta name="Author" content="codeCrystalgeysers">
+<meta name="Keywords" content="HALCinema, ハルシネマ, 映画, えいが">
+<link rel="stylesheet" type="text/css" href="css/index.css">
+<title>HAL Cinema</title>
+
+<?php
+include 'head.php';
+?>
+
+<div id="slider">
+    映画のスライドがここに出ます
+</div>
+<div id="reserveArea">
+    座席，人物アイコンやらここに
+    <div id="sheet">
+        席が選べるやつな　キャパシティ越えるやつな
+    </div>
+    <div id="details">
+        <dl>
+            <dt>選択映画タイトルでる</dt>
+            <dl><img src="nowImg"></dl>
+            <dl>詳細情報</dl>
+        </dl>
+        p#now
+        <p id="nowPrice">料金</p>
+        
+        料金とかでる
+    </div>
+</div>
+
+<?php
+include 'foot.php';
