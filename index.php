7a49b39403e496e7f4f961de3167a7eaef97e679
MikiWakana <o-ce@o-ce.local>
2014-07-31 20:13:27 +0900

    v.p
    main

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<meta charset="UTF-8">
+<meta name="Author" content="codeCrystalgeysers">
+<meta name="Keywords" content="HALCinema, ハルシネマ, 映画, えいが">
+<link rel="stylesheet" type="text/css" href="css/common.css">
+<link rel="stylesheet" type="text/css" href="css/index.css">
+<title>HAL Cinema</title>
+
+<?php
+include 'head.php';
+?>
+
+<?php
+include 'foot.php';
