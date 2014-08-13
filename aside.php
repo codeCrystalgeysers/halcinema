18dac99792945840959321ced65981e030838f49
mil-cre-mar <tcnk227@gmail.com>
2014-08-14 02:18:51 +0900

    修正

--- aside.php
+++ aside.php
@@ -2,42 +2,55 @@
 <meta charset="UTF-8">
 <meta name="Author" content="codeCrystalgeysers">
 <meta name="Keywords" content="HALCinema, ハルシネマ, 映画, えいが">
-<script src="js/jq2min.js"></script>
-<script src="js/topSlider.js"></script>
 <link rel="stylesheet" type="text/css" href="css/common.css">
-<link rel="stylesheet" type="text/css" href="css/index.css">
 <link rel="stylesheet" type="text/css" href="css/aside.css">
 <title>HAL Cinema</title>
+<?php
+    include 'head.php';
+?>
 
+<main>
+    <div id="menu">
+		<div id="reservation">
+			チケット予約
+		</div><!--reservation-->
+	
+		<div id="phoneReservation">
+			チケット予約
+		</div><!--phoneReservation-->
+	
+		<table border="1">
+			<tr>
+				<th>映画総選挙</th>
+			</tr>
+			<tr>
+				<td>1位○○○○○○</td>
+			</tr>
+			<tr>
+				<td>2位○○○○○○</td>
+			</tr>
+			<tr>
+				<td>3位○○○○○○</td>
+			</tr>
+			<tr>
+				<td>締め切りまであと○○日</td>
+			</tr>
+		</table>
+	
+		<div id="publicity">
+			広告・バナー
+		</div><!--publicity-->
+	</div><!--menu-->
+</main>
 
-<div id="menu">
-    <div id="reservation">
-        チケット予約
-    </div><!--reservation-->
-
-    <div id="phoneReservation">
-        チケット予約
-    </div><!--phoneReservation-->
-
-    <table border="1">
-        <tr>
-            <th>映画総選挙</th>
-        </tr>
-        <tr>
-            <td>1位○○○○○○</td>
-        </tr>
-        <tr>
-            <td>2位○○○○○○</td>
-        </tr>
-        <tr>
-            <td>3位○○○○○○</td>
-        </tr>
-        <tr>
-            <td>締め切りまであと○○日</td>
-        </tr>
-    </table>
-
-    <div id="publicity">
-        広告・バナー
-    </div><!--publicity-->
-</div><!--menu-->
+<div id="divide">
+    <aside id="subContent">
+    <?php
+        include 'aside.php';
+    ?>
+    </aside>
+    <div id="links"><!-- 広告 --></div>
+</div>
+<?php
+    include 'foot.php';
+?>
--- help.php
+++ help.php
@@ -2,7 +2,7 @@
 <meta charset="UTF-8">
 <meta name="Author" content="codeCrystalgeysers">
 <meta name="Keywords" content="HALCinema, ハルシネマ, 映画, えいが">
-<link rel="stylesheet" type="text/css" href="css/index.css">
+<link rel="stylesheet" type="text/css" href="css/common.css">
 <link rel="stylesheet" type="text/css" href="css/help.css">
 <title>HAL Cinema</title>
 <?php
