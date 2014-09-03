2983111a0f15b122c9e3419ca57c5c338f7ce968
okaoka3 <niiokatoyo@yahoo.co.jp>
2014-09-03 18:21:23 +0900

    登録確認ページ直し

---
+++ registrationCheck.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<meta charset="UTF-8">
+<meta name="Author" content="codeCrystalgeysers">
+<meta name="Keywords" content="HALCinema, ハルシネマ, 映画, えいが">
+<link rel="stylesheet" type="text/css" href="css/common.css">
+<link rel="stylesheet" type="text/css" href="css/registrationCheck.css">
+<title>HAL Cinema</title>
+<?php
+    include 'head.php';
+?>
+
+<main>
+
+    <h2>新規会員登録確認画面</h2>
+    <div class="formArea">
+    	<table>
+    		<tr>
+    			<td class="">※性名</td>
+                <td>姓&nbsp;：&nbsp;<span>土屋</span>&emsp;名&nbsp;：&nbsp;<span>新保</span></td>
+    		</tr>
+    		<tr>
+    			<td>※フリガナ</td>
+                <td><span>セイ&nbsp;：&nbsp;ツチヤ</span>&emsp;<span>メイ&nbsp;：&nbsp;シンボ</span></td>
+    		</tr>
+    		<tr>
+    			<td>※性別</td>
+    			<td>
+                    <span>男女</span>
+				</td>
+    		</tr>
+    		<tr>
+    			<td>※生年月日</td>
+                <td>
+                	<span>2014</span>年
+                    <span>9</span>月
+                    <span>3</span>日
+				</td>
+    		</tr>
+    		<tr>
+    			<td>※郵便番号</td><td>〒<span>030-3434</span></td>
+    		</tr>
+    		<tr>
+    			<td>※都道府県</td><td><span>東京</span></td>
+	    	</tr>
+    		<tr>
+    			<td>※市区町村番地</td><td><span>新宿区西新宿７－７－７</span></td>
+    		</tr>
+    		<tr>
+    			<td>※マンション名</td><td><span>コクーンタワー</span></td>
+    		</tr>
+    		<tr>
+    			<td>※ログインID</td><td><span>gotoh</span></td>
+    		</tr>
+    		<tr>
+    			<td>※パスワード</td><td><span>jdfijifkf;l</span></td>
+    		</tr>
+
+    			<td>※電話番号</td><td><span>03-9898-9889</span></td>
+    		</tr>
+    	</table>
+    </div>
+
+    <div class="formArea">
+    <h2>クレジットカード情報</h2>
+    	<table>
+    		<tr>
+    			<td>カード会社</td>
+    			<td><span>life</span></td>
+    		</tr>
+    		<tr>
+    			<td>カード名義</td><td><span>sinji gotoh</span></td>
+    		</tr>
+    		<tr>
+    			<td>カード番号</td><td><span>456777778788</span></td>
+    		</tr>
+    		<tr>
+    			<td>有効期限</td>
+                <td>
+                	<span>3</span>月
+    				<span>2017</span>年
+               	</td>
+    		</tr>
+    	</table>
+    </div>
+
+    <div id="conf">
+    	<input type="button" value="この内容で登録する">
+    </div>
+
+</main>
+
+
+<?php
+    include 'foot.php';
+?>
