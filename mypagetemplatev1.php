4fc7b98e95e90f6f61c1bcefb58894182c5431fa
okaoka3 <niiokatoyo@yahoo.co.jp>
2014-09-10 11:41:11 +0900

    no message

---
+++ mypagetemplatev1.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<meta charset="UTF-8">
+<meta name="Author" content="codeCrystalgeysers">
+<meta name="Keywords" content="HALCinema, ハルシネマ, 映画, えいが">
+<link rel="stylesheet" type="text/css" href="css/common.css">
+<link rel="stylesheet" type="text/css" href="css/mypagecommon.css">
+<link rel="stylesheet" type="text/css" href="css/index.css">
+<title>HAL Cinema</title>
+<?php
+    include 'head.php';
+?>
+
+<div id="left">
+    <aside id="localMenu">
+    <?php
+        include 'mypageaside.php';
+    ?>
+    </aside>
+    <div id="links"><!-- 広告 --></div>
+</div>
+
+<main id="right">
+    ここを編集
+</main>
+
+<?php
+    include 'foot.php';
+?>
