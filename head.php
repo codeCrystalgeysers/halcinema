3b04e0fd93f5e3cb9f98ca6ecf4ef4c9ec5f8066
mil-cre-mar <tcnk227@gmail.com>
2014-11-18 12:44:03 +0900

    ヘッダーのログインまわり
    あ

--- head.php
+++ head.php
@@ -13,11 +13,28 @@
           <!-- どう表示だっけ?別ページへリンクある? -->Alert
       </div>
       <div id="loginWrapper">
-          <button id="loginBtnIn">ログイン</button>
+<?php
+	//セッション開始
+	session_start();
+
+	//セッションチェック
+	if(!isset($_SESSION["id"])){			
+?>
+		<form action="login.php">
+        	<button id="loginBtnIn">ログイン</button>
+		</form>
+<?php
+	}else{
+?>
           <div id="loginNowIn">
-              <p>IDまたは名前表示</p>
-              <button id="loginBtnOut">ログアウト</button>
+              <p><?= $_SESSION["id"] ?></p>
+			  <form action="logout.php">
+	              <button id="loginBtnOut">ログアウト</button>
+              </form>
           </div>
+<?php
+	}
+?>
       </div>
       <nav id="headerMenu">
           <ul id="headerMenuList">
@@ -46,55 +63,5 @@
           </ul>
       </nav>
     </div>
-    <div id="login">
-<?php
-	//セッション開始
-	session_start();
-
-	//セッションチェック
-	if(!isset($_SESSION["id"])){			
-?>
-	    <form action="login.php">
-            <button id="btnLogin">ログイン</button>
-        </form>
-<?php
-	}else{
-?>
-        <div id="nowLogin">
-            <p><?= $_SESSION["id"] ?></p>
-			<form action="logout.php">
-            	<button id="btnLogout">ログアウト</button>
-			</form>
-        </div>
-<?php
-	}
-?>
-    </div>
-    <nav id="headMenu">
-        <ul>
-            <li><a href="index.php">HOME</a></li>
-            <li><a href="article.php">上映情報</a>
-                <ul class="subMenuList">
-                    <li><a href="#">スケジュール</a></li>
-                    <li><a href="#">作品情報・イベント</a></li>
-                </ul>
-            </li>
-            <li><a href="theater.php">インフォメーション</a>
-                <ul class="subMenuList">
-                    <li><a href="#">お知らせ</a></li>
-                    <li><a href="#">施設案内</a></li>
-                    <li><a href="#">上映設備</a></li>
-                </ul>
-            </li>
-            <li><a href="price.php">料金</a>
-                <ul class="subMenuList">
-                    <li><a href="#">チケット</a></li>
-                    <li><a href="#">グッズ</a></li>
-                    <li><a href="#">フード・ドリンク</a></li>
-                </ul>
-            </li>
-            <li><a href="access.php">アクセス</a></li>
-        </ul>
-    </nav>
 </header>
 <div id="wrapper">
