c28dccdb97dbb0755bc1338dbc315152738c9719
okaoka3 <niiokatoyo@yahoo.co.jp>
2014-11-25 20:10:34 +0900

    マイページと登録情報変更ページの見た目

--- mypage.php
+++ mypage.php
@@ -3,104 +3,82 @@
 <meta name="Author" content="codeCrystalgeysers">
 <meta name="Keywords" content="HALCinema, ハルシネマ, 映画, えいが">
 <link rel="stylesheet" type="text/css" href="css/common.css">
+<link rel="stylesheet" type="text/css" href="css/mypagecommon.css">
 <link rel="stylesheet" type="text/css" href="css/mypage.css">
+
 <title>HAL Cinema</title>
 <?php
     include 'head.php';
 ?>
 
-<main>
+<div id="contentsWrap">
+    <div id="left">
+        <aside id="localMenu">
+            <?php
+                include 'mypageaside.php';
+            ?>
+        </aside>
+        <div id="links">
+            <div class="button">
+                チケット予約
+            </div>
 
-    <div id="leftColumn">
+            <div class="button">
+                スマートフォンでの予約はこちら
+            </div>
 
-    	<div class="title">座席予約中作品</div>
+            <table id="movieElection">
+                <tr>
+                    <td>○/○　映画総選挙途中経過</td>
+                </tr>
+                <tr>
+                    <td>1位　○○○○○○○</td>
+                </tr>
+                <tr>
+                    <td>2位　○○○○○○○</td>
+                </tr>
+                <tr>
+                    <td>3位　○○○○○○○</td>
+                </tr>
+            </table>
 
-    	<table border="1" class="reservation">
-    		<tr>
-    			<td>作品名</td><td>上映日時</td><td>SCREEN</td><td>支払い方法</td><td>予約日</td>
-    		</tr>
-    		<tr>
-    			<td>ゴジラ</td><td>9/3(水)</td><td>SCREEN⑤</td><td>クレジットカード</td><td>8/31（日）</td>
-    		</tr>
-    	</table>
+            <div id="bana">
+                広告・バナー
+            </div>
+        </div>
+    </div>
 
-    	<table border="1" class="reservation">
-    		<tr>
-    			<td>作品名</td><td>上映日時</td><td>SCREEN</td><td>支払い方法</td><td>予約日</td>
-    		</tr>
-    		<tr>
-    			<td>ゴジラ</td><td>9/4(木)</td><td>SCREEN⑤</td><td>クレジットカード</td><td>8/31（日）</td>
-    		</tr>
-    	</table>
+    <main id="right">
+        <div class="title">座席予約中作品</div>
 
-    	<table border="1" class="reservation">
-    		<tr>
-    			<td>作品名</td><td>上映日時</td><td>SCREEN</td><td>支払い方法</td><td>予約日</td>
-    		</tr>
-    		<tr>
-    			<td>ゴジラ</td><td>9/5(金)</td><td>SCREEN⑤</td><td>クレジットカード</td><td>8/31（日）</td>
-    		</tr>
-    	</table>
+        <table class="reservation">
+            <tr>
+                <th>作品名</th><th>上映日時</th><th>SCREEN</th><th>支払い方法</th><th>予約日</th>
+            </tr>
+            <tr>
+                <td>ゴジラ</td><td>9/3(水)</td><td>SCREEN⑤</td><td>クレジットカード</td><td>8/31（日）</td>
+            </tr>
+            <tr>
+                <td>ゴジラ</td><td>9/4(木)</td><td>SCREEN⑤</td><td>クレジットカード</td><td>8/31（日）</td>
+            </tr>
+            <tr>
+                <td>ゴジラ</td><td>9/5(金)</td><td>SCREEN⑤</td><td>クレジットカード</td><td>8/31（日）</td>
+            </tr>
+        </table>
 
-	    <br />
-	    ・<br />
-	    ・<br />
-	    ・<br />
+        <div class="title">購入履歴</div>
 
-    	<div class="title">購入履歴</div>
-
-    	<table border="1" class="reservation">
-    		<tr>
-    			<td>作品名</td><td>上映日時</td><td>SCREEN</td><td>支払い方法</td><td>予約日</td>
-    		</tr>
-    		<tr>
-    			<td>ゴジラ</td><td>9/2(火)</td><td>SCREEN⑤</td><td>クレジットカード</td><td>8/31（日）</td>
-    		</tr>
-    	</table>
-
-	</div><!--leftColumn-->
-
-	<div id="rightColumn">
-
-		<div class="button">
-			チケット予約
-		</div>
-
-		<div class="button">
-			スマートフォンでの予約はこちら
-		</div>
-
-		<table id="movieElection">
-			<tr>
-				<td>○/○　映画総選挙途中経過</td>
-			<tr>
-			<tr>
-				<td>1位　○○○○○○○</td>
-			<tr>
-			<tr>
-				<td>2位　○○○○○○○</td>
-			<tr>
-			<tr>
-				<td>3位　○○○○○○○</td>
-			<tr>
-		</table>
-
-		<div id="bana">
-			広告・バナー
-		</div>
-
-	</div><!--rightColumn-->
-
-</main>
-
-<div id="divide">
-    <aside id="subContent">
-    <?php
-        include 'aside.php';
-    ?>
-    </aside>
-    <div id="links"><!-- 広告 --></div>
+        <table class="reservation">
+            <tr>
+                <th>作品名</th><th>上映日時</th><th>SCREEN</th><th>支払い方法</th><th>予約日</th>
+            </tr>
+            <tr>
+                <td>ゴジラ</td><td>9/2(火)</td><td>SCREEN⑤</td><td>クレジットカード</td><td>8/31（日）</td>
+            </tr>
+        </table>
+    </main>
 </div>
+
 <?php
     include 'foot.php';
 ?>
--- registrationChange.php
+++ registrationChange.php
@@ -3,6 +3,7 @@
 <meta name="Author" content="codeCrystalgeysers">
 <meta name="Keywords" content="HALCinema, ハルシネマ, 映画, えいが">
 <link rel="stylesheet" type="text/css" href="css/common.css">
+<link rel="stylesheet" type="text/css" href="css/mypagecommon.css">
 <link rel="stylesheet" type="text/css" href="css/registrationChange.css">
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <title>HAL Cinema</title>
@@ -22,237 +23,233 @@ function action(n){
 
 </script>
 
-<main>
+<div id="contentsWrap">
+    <div id="left">
+            <aside id="localMenu">
+                <?php
+                    include 'mypageaside.php';
+                ?>
+            </aside>
+    </div>
 
-    <h2>登録変更画面</h2>
-    <div class="formArea">
-        <div class="box">
-            <div class="left1">
-                ※姓名
+    <main id="right">
+        <h2>登録変更画面</h2>
+        <div class="formArea">
+            <div class="box">
+                <div class="left1">
+                    ※姓名
+                </div>
+                <div class="left2">
+                    本間芽衣子
+                </div>
+                <div class="left3">
+                    <input class="toggleBtn" type="button" value="変更" onclick="action(1)" />
+                </div>
+                <div id="change1" class="change">
+                    姓：<input type="text" />名：<input type="text" />
+                    <br />
+                    <input type="button" value="変更する" />
+                </div>
             </div>
-            <div class="left2">
-                本間芽衣子
-            </div>
-            <div class="left3">
-                <input type="button" value="変更" onclick="action(1)" />
-            </div>
-        </div>
 
-        <div id="change1" class="change" />
-        	姓：<input type="text" />名：<input type="text" />
-        	<br />
-        	<input type="button" value="変更する" />
-       </div>
+            <div class="box">
+                <div class="left1">
+                    ※フリガナ
+                </div>
+                <div class="left2">
+                    ホンマメイコ
+                </div>
+                <div class="left3">
+                    <input class="toggleBtn" type="button" value="変更" onclick="action(2)" />
+                </div>
+                <div id="change2" class="change">
+                    セイ：<input type="text" />メイ：<input type="text" />
+                    <br />
+                    <input type="button" value="変更する" />
+                </div>
+            </div>
 
-        <div class="box">
-            <div class="left1">
-                ※フリガナ
+            <div class="box">
+                <div class="left1">
+                    ※性別
+                </div>
+                <div class="left2">
+                    女
+                </div>
+                <div class="left3">
+                    <input class="toggleBtn" type="button" value="変更" onclick="action(3)" />
+                </div>
+                <div id="change3" class="change">
+                    <label><input type="radio" name="sex" value="男" />男</label>
+                    <label><input type="radio" name="sex" value="女" />女</label>
+                    <br />
+                    <input type="button" value="変更する" />
+                </div>
             </div>
-            <div class="left2">
-                ホンマメイコ
-            </div>
-            <div class="left3">
-                <input type="button" value="変更" onclick="action(2)" />
-            </div>
-        </div>
 
-        <div id="change2" class="change" />
-        	セイ：<input type="text" />メイ：<input type="text" />
-        	<br />
-        	<input type="button" value="変更する" />
-       </div>
+            <div class="box">
+                <div class="left1">
+                    ※生年月日
+                </div>
+                <div class="left2">
+                    1994年9月18日
+                </div>
+                <div class="left3">
+                    <input class="toggleBtn" type="button" value="変更" onclick="action(4)" />
+                </div>
+                <div id="change4" class="change">
+                    <select name="year">
+                        <?php
+                            $nowYear = date("Y");
+                            for($i=1970;$i<=$nowYear;$i++){
+                                echo "<option value=".$i.">".$i."</option>";
+                            }
+                        ?>
+                    </select>年
+                    <select name="month">
+                    <?php
+                            for($i=1;$i<13;$i++){
+                                echo "<option value=".$i.">".$i."</option>";
+                            }
+                        ?>
+                    </select>月
+                    <select name="day">
+                        <?php
+                            for($i=1;$i<32;$i++){
+                                echo "<option value=".$i.">".$i."</option>";
+                            }
+                        ?>
+                    </select>日
+                    <br />
+                    <input type="button" value="変更する" />
+                </div>
+            </div>
 
-        <div class="box">
-            <div class="left1">
-                ※性別
+            <div class="box">
+                <div class="left1">
+                    ※郵便番号
+                </div>
+                <div class="left2">
+                    〒332-0023
+                </div>
+                <div class="left3">
+                    <input class="toggleBtn" type="button" value="変更" onclick="action(5)" />
+                </div>
+                <div id="change5" class="change">
+                    〒<input type="text" />
+                    <br />
+                    <input type="button" value="変更" />
+                </div>
             </div>
-            <div class="left2">
-                女
-            </div>
-            <div class="left3">
-                <input type="button" value="変更" onclick="action(3)" />
-            </div>
-        </div>
 
-        <div id="change3" class="change" />
-            <input type="radio" name="sex" value="男" />男
-            <input type="radio" name="sex" value="女" />女
-            <br />
-        	<input type="button" value="変更する" />
-       </div>
+             <div class="box">
+                <div class="left1">
+                    ※都道府県
+                </div>
+                <div class="left2">
+                    埼玉県
+                </div>
+                <div class="left3">
+                    <input class="toggleBtn" type="button" value="変更" onclick="action(6)" />
+                </div>
+                <div id="change6" class="change">
+                    <select name="state">
+                        <option value="">ﾄﾁｷﾞ</option>
+                        <option value="">埼玉</option>
+                        <option value="">ｸﾞﾝﾏ</option>
+                    </select>
+                    <br />
+                    <input type="button" value="変更" />
+                </div>
+            </div>
 
-        <div class="box">
-            <div class="left1">
-                ※生年月日
+            <div class="box">
+                <div class="left1">
+                    ※市区町村番地
+                </div>
+                <div class="left2">
+                    秩父市1丁目
+                </div>
+                <div class="left3">
+                    <input class="toggleBtn" type="button" value="変更" onclick="action(7)" />
+                </div>
+                <div id="change7" class="change">
+                    <input type="text" />
+                    <br />
+                    <input type="button" value="変更する" />
+                </div>
             </div>
-            <div class="left2">
-                1994年9月18日
-            </div>
-            <div class="left3">
-                <input type="button" value="変更" onclick="action(4)" />
-            </div>
-        </div>
 
-        <div id="change4" class="change" />
-        	<select name="year">
-	    		<?php
-					$nowYear = date("Y");
-    				for($i=1970;$i<=$nowYear;$i++){
-    					echo "<option value=".$i.">".$i;
-    				}
-    			?>
-	   		</select>年
-    		<select name="month">
-	    		<?php 
-    				for($i=1;$i<13;$i++){
-    					echo "<option value=".$i.">".$i;
-    				}
-    			?>
-    		</select>月
-    		<select name="day">
-	    		<?php
-    				for($i=1;$i<32;$i++){
-    					echo "<option value=".$i.">".$i;
-    				}
-    			?>
-    		</select>日
-    		<br />
-    		<input type="button" value="変更する" />
-	    </div>
+            <div class="box">
+                <div class="left1">
+                    ※マンション名
+                </div>
+                <div class="left2">
+                    河合荘203号
+                </div>
+                <div class="left3">
+                    <input class="toggleBtn" type="button" value="変更" onclick="action(8)" />
+                </div>
+                <div id="change8" class="change">
+                    <input type="text" />
+                    <br />
+                    <input type="button" value="変更する" />
+                </div>
+            </div>
 
-        <div class="box">
-            <div class="left1">
-                ※郵便番号
+            <div class="box">
+                <div class="left1">
+                    ※ログインID
+                </div>
+                <div class="left2">
+                    mennmatan
+                </div>
+                <div class="left3">
+                    <input class="toggleBtn" type="button" value="変更" onclick="action(9)" />
+                </div>
+                <div id="change9" class="change">
+                    <input type="text" />
+                    <br />
+                    <input type="button" value="変更する" />
+                </div>
             </div>
-            <div class="left2">
-                〒332-0023
-            </div>
-            <div class="left3">
-                <input type="button" value="変更" onclick="action(5)" />
-            </div>
-        </div>
 
-        <div id="change5" class="change" />
-        	〒<input type="text" />
-	    	<br />
-	    	<input type="button" value="変更" />
-       </div>
+            <div class="box">
+                <div class="left1">
+                    ※パスワード
+                </div>
+                <div class="left2">
+                    **********
+                </div>
+                <div class="left3">
+                    <input class="toggleBtn" type="button" value="変更" onclick="action(10)" />
+                </div>
+                <div id="change10" class="change">
+                    <input type="text" />
+                    <br />
+                    <input type="button" value="変更する" />
+               </div>
+            </div>
 
-         <div class="box">
-            <div class="left1">
-                ※都道府県
+            <div class="box">
+                <div class="left1">
+                    ※電話番号
+                </div>
+                <div class="left2">
+                    0120441222
+                </div>
+                <div class="left3">
+                    <input class="toggleBtn" type="button" value="変更" onclick="action(11)" />
+                </div>
+                <div id="change11" class="change">
+                    <input type="text" />
+                    <br />
+                    <input type="button" value="変更する" />
+               </div>
             </div>
-            <div class="left2">
-                埼玉県
-            </div>
-            <div class="left3">
-                <input type="button" value="変更" onclick="action(6)" />
-            </div>
-        </div>
-
-        <div id="change6" class="change" />
-        	<select name="state">
-	    		<option value="">ﾄﾁｷﾞ</option>
-	    		<option value="">埼玉</option>
-	    		<option value="">ｸﾞﾝﾏ</option>
-	    	</select>
-	    	<br />
-	    	<input type="button" value="変更" />
-       </div>
-
-         <div class="box">
-            <div class="left1">
-                ※市区町村番地
-            </div>
-            <div class="left2">
-                秩父市1丁目
-            </div>
-            <div class="left3">
-                <input type="button" value="変更" onclick="action(7)" />
-            </div>
-        </div>
-
-        <div id="change7" class="change" />
-        	<input type="text" />
-        	<br />
-        	<input type="button" value="変更する" />
-       </div>
-
-         <div class="box">
-            <div class="left1">
-                ※マンション名
-            </div>
-            <div class="left2">
-                河合荘203号
-            </div>
-            <div class="left3">
-                <input type="button" value="変更" onclick="action(8)" />
-            </div>
-        </div>
-
-        <div id="change8" class="change" />
-        	<input type="text" />
-        	<br />
-        	<input type="button" value="変更する" />
-       </div>
-
-         <div class="box">
-            <div class="left1">
-                ※ログインID
-            </div>
-            <div class="left2">
-                mennmatan
-            </div>
-            <div class="left3">
-                <input type="button" value="変更" onclick="action(9)" />
-            </div>
-        </div>
-
-        <div id="change9" class="change" />
-        	<input type="text" />
-        	<br />
-        	<input type="button" value="変更する" />
-       </div>
-
-         <div class="box">
-            <div class="left1">
-                ※パスワード
-            </div>
-            <div class="left2">
-                **********
-            </div>
-            <div class="left3">
-                <input type="button" value="変更" onclick="action(10)" />
-            </div>
-        </div>
-
-        <div id="change10" class="change" />
-        	<input type="text" />
-        	<br />
-        	<input type="button" value="変更する" />
-       </div>
-
-         <div class="box">
-            <div class="left1">
-                ※電話番号
-            </div>
-            <div class="left2">
-                0120441222
-            </div>
-            <div class="left3">
-                <input type="button" value="変更" onclick="action(11)" />
-            </div>
-        </div>
-
-        <div id="change11" class="change" />
-        	<input type="text" />
-        	<br />
-        	<input type="button" value="変更する" />
-       </div>
-
-    </div><!--formArea-->
-</main>
-
+        </div><!--formArea-->
+    </main>
+</div>
 
 <?php
     include 'foot.php';
