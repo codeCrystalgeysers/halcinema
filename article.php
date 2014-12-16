016b83f21a1e00cfb946943663d6159438d54da4
こたに <shbsk0508@yahoo.co,jp>
2014-12-16 10:30:48 +0900

    a
    a

--- article.php
+++ article.php
@@ -13,36 +13,24 @@
 <?php
     include 'head.php';
 ?>
+  
 
-<<<<<<< HEAD
-        <ul class="days">
-    		<li><img src="img/img1.jpg"></li>
-    		<li><img src="img/img2.jpg"></li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-            <li>6</li>
-            <li>7</li>
-            
-=======
 <div id="contentsWrap">
     <main>
     	<div class="ttt">上映スケジュール</div>
     	<div id="day">
     		<div id="back">
     		</div>
->>>>>>> origin/master
 
             <ul class="days">
         		<li><img src="img/img1.jpg"></li>
-        		<li>2</li>
-                <li>3</li>
-                <li>4</li>
-                <li>5</li>
-                <li>6</li>
-                <li>7</li>
-
-
+        		<li><img src="img/img2.jpg"></li>
+                <li><img src="img/img1.jpg"></li>
+                <li><img src="img/img2.jpg"></li>
+                <li><img src="img/img1.jpg"></li>
+                <li><img src="img/img2.jpg"></li>
+                <li><img src="img/img1.jpg"></li>
+                <li><img src="img/img2.jpg"></li>
             </ul>
 
     		<div id="next">
@@ -53,43 +41,17 @@
     	       作品名
     	</div>
 
-    	<ul id="lst">
-    		<li id="movietime">←</li>
-            <li id="movietime">11:00～<br />△</li>
-            <li id="movietime">13:00～<br />△</li>
-            <li id="movietime">15:00～<br />△</li>
-            <li id="movietime">17:00～<br />△</li>
-            <li id="movietime">→</li>
+    	<ul class="lst">
+            <li class="movietime">11:00～<br />△</li>
+            <li class="movietime">13:00～<br />△</li>
+            <li class="movietime">15:00～<br />△</li>
+            <li class="movietime">17:00～<br />△</li>
         </ul>
-        <ul id="lst">
-    		<li id="movietime">←</li>
-            <li id="movietime">11:00～<br />△</li>
-            <li id="movietime">13:00～<br />△</li>
-            <li id="movietime">15:00～<br />△</li>
-            <li id="movietime">17:00～<br />△</li>
-            <li id="movietime">→</li>
-        </ul>
-
-        <div class="ttt">
-    		作品名
-    	</div>
-
-    	<ul id="lst">
-    		<li id="movietime">←</li>
-            <li id="movietime">11:00～<br />△</li>
-            <li id="movietime">13:00～<br />△</li>
-            <li id="movietime">15:00～<br />△</li>
-            <li id="movietime">17:00～<br />△</li>
-            <li id="movietime">→</li>
-        </ul>
-
-        <ul id="lst">
-    		<li id="movietime">←</li>
-            <li id="movietime">11:00～<br />△</li>
-            <li id="movietime">13:00～<br />△</li>
-            <li id="movietime">15:00～<br />△</li>
-            <li id="movietime">17:00～<br />△</li>
-            <li id="movietime">→</li>
+        <ul class="lst">
+            <li class="movietime">11:00～<br />△</li>
+            <li class="movietime">13:00～<br />△</li>
+            <li class="movietime">15:00～<br />△</li>
+            <li class="movietime">17:00～<br />△</li>
         </ul>
     </main>
 
