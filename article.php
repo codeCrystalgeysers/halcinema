1a54cfd4101b143b5e07f056979fc12475af86df
こたに <shbsk0508@yahoo.co,jp>
2014-12-16 21:16:15 +0900

    aa
    aaa

--- article.php
+++ article.php
@@ -12,63 +12,78 @@
 <title>HAL Cinema</title>
 <?php
     include 'head.php';
-	
-	$todayYear = date("Y");
-	$todayMonth = date("n");
-	$todayDay = date("d");
-	$todayWeek = date("w");
-	$today = $todayYear.$todayMonth.$todayDay;
-	
-	$week = array("日","月","火","水","木","金","土");
-	
-	//DB接続
-	$con = mysql_connect("localhost","root","");
-	
-	mysql_query("SET NAMES utf8");
+    
+    $todayYear = date("Y");
+    $todayMonth = date("n");
+    $todayDay = date("d");
+    $todayWeek = date("w");
+    $today = $todayYear.$todayMonth.$todayDay;
+    
+    $week = array("日","月","火","水","木","金","土");
+    
+    //DB接続
+    $con = mysql_connect("localhost","root","");
+    
+    mysql_query("SET NAMES utf8");
 
-	//DB選択
-	mysql_select_db("halcinema",$con);
-	
-	//SQL作成
-	$sql = "SELECT movie_id,title,image FROM movie WHERE '$today' >= opening_day && '$today' <= end_day";
-	$sql2 = "SELECT count(*) FROM movie WHERE '$today' >= opening_day && '$today' <= end_day";
-	//SQL実行
-	$res = mysql_query($sql,$con);
-	$count = mysql_query($sql2,$con);
+    //DB選択
+    mysql_select_db("halcinema",$con);
+    
+    //SQL作成
+    $sql = "SELECT movie_id,title,image FROM movie WHERE '$today' >= opening_day && '$today' <= end_day";
+    $sql2 = "SELECT count(*) FROM movie WHERE '$today' >= opening_day && '$today' <= end_day";
+    //SQL実行
+    $res = mysql_query($sql,$con);
+    $count = mysql_query($sql2,$con);
 ?>
-  
- <div id="contentsWrap">
+
+<div id="contentsWrap">
     <main>
     	<div class="ttt">上映スケジュール</div>
     	<div id="day">
-		
+
             <ul class="days">
-<?php
-	while($row = mysql_fetch_array($res)){
-?>
-				<li><img src='img/<?= $row["image"] ?>.jpg' id='<?= $row["movie_id"] ?>'></li>
-<?php
-	}
-?>
+        		<li>1/2</li>
+        		<li>1/2</li>
+                <li>1/2</li>
+                <li>1/2</li>
+                <li>1/2</li>
+                <li>1/2</li>
+                <li>1/2</li>
             </ul>
+
     	</div>
 
-    	<div class="ttt">
-    	      <!-- 作品名 -->
+    	<div class="sttt">
+    	       作品名
     	</div>
-
+        <div class="screenimg">
+            <div class="iimmgg">
+                画像
+            </div>
+        </div>
     	<ul class="lst">
-            <li class="movietime">11:00～<br />△</li>
-            <li class="movietime">13:00～<br />△</li>
-            <li class="movietime">15:00～<br />△</li>
-            <li class="movietime">17:00～<br />△</li>
+            <li class="screen">シアター1</li>
+            <li class="movietime">13:00～</li>
+            <li class="movietime">15:00～</li>
+            <li class="movietime">17:00～</li>
         </ul>
         <ul class="lst">
-            <li class="movietime">11:00～<br />△</li>
-            <li class="movietime">13:00～<br />△</li>
-            <li class="movietime">15:00～<br />△</li>
-            <li class="movietime">17:00～<br />△</li>
+            <li class="screen">シアター3</li>
+            <li class="movietime">13:00～</li>
+            <li class="movietime">15:00～</li>
+            <li class="movietime">17:00～</li>
+            <li class="movietime">17:00～</li>
+            <li class="movietime">17:00～</li>
         </ul>
+
+        <ul class="lst">
+            <li class="screen">シアター5</li>
+            <li class="movietime">13:00～</li>
+            <li class="movietime">15:00～</li>
+            <li class="movietime">17:00～</li>
+        </ul>
+
     </main>
 
     <div id="divide">
