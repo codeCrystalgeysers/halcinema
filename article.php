bf4aa44ea90c9d3e091719171bf128417dec161e
mil-cre-mar <tcnk227@gmail.com>
2014-12-16 21:07:42 +0900

    すけ
    a

--- article.php
+++ article.php
@@ -12,6 +12,29 @@
 <title>HAL Cinema</title>
 <?php
     include 'head.php';
+	
+	$todayYear = date("Y");
+	$todayMonth = date("n");
+	$todayDay = date("d");
+	$todayWeek = date("w");
+	$today = $todayYear.$todayMonth.$todayDay;
+	
+	$week = array("日","月","火","水","木","金","土");
+	
+	//DB接続
+	$con = mysql_connect("localhost","root","");
+	
+	mysql_query("SET NAMES utf8");
+
+	//DB選択
+	mysql_select_db("halcinema",$con);
+	
+	//SQL作成
+	$sql = "SELECT movie_id,title,image FROM movie WHERE '$today' >= opening_day && '$today' <= end_day";
+	$sql2 = "SELECT count(*) FROM movie WHERE '$today' >= opening_day && '$today' <= end_day";
+	//SQL実行
+	$res = mysql_query($sql,$con);
+	$count = mysql_query($sql2,$con);
 ?>
   
 
@@ -33,14 +56,14 @@
 >>>>>>> origin/master
 
             <ul class="days">
-        		<li><img src="img/img1.jpg"></li>
-        		<li><img src="img/img2.jpg"></li>
-                <li><img src="img/img1.jpg"></li>
-                <li><img src="img/img2.jpg"></li>
-                <li><img src="img/img1.jpg"></li>
-                <li><img src="img/img2.jpg"></li>
-                <li><img src="img/img1.jpg"></li>
-                <li><img src="img/img2.jpg"></li>
+<?php
+	while($row = mysql_fetch_array($res)){
+?>
+				<li><img src='img/<?= $row["image"] ?>.jpg' id='<?= $row["movie_id"] ?>'></li>
+<?php
+	}
+?>
+
             </ul>
 
     		<div id="next">
@@ -48,7 +71,7 @@
     	</div>
 
     	<div class="ttt">
-    	       作品名
+    	      <!-- 作品名 -->
     	</div>
 
     	<ul class="lst">
