23a7b42cef1dcd02c7fa4c33abfc088c0d760a29
じゅりっぺ <jyury.cyury0314@gmail.com>
2014-12-17 21:12:48 +0900

    ｳｪｲ

--- theater.php
+++ theater.php
@@ -11,8 +11,8 @@
 
 <div id="contentsWrap">
 	<main>
-		<div class="ttt">TOPICS</div>
 		<div id="box1">
+			<div class="ttt">TOPICS</div>
 			<div class="h4">大人がアニメに泣かされる！『あの花』がついに映画化</div>
 			<p>今夏も続々とお化けが登場する映画が公開されていますね。ホラー映画は嫌いだから観ない！なんて思ったあなた、『劇場版 あの日見た花の名前を僕達はまだ知らない。』は、感動の涙を流してしまう映画なのです。</p>
 			<img src="img/img1.jpg">
@@ -21,31 +21,36 @@
 			<div class="h5">ストーリー</div>
 			<p>宿海仁太こと、じんたんを中心とする仲良し6人組の小学生「超平和バスターズ」は、メンバーの1人であるめんま（本間芽衣子）の死をきっかけに疎遠になっていた。高校一年生になったある日、めんまの死というトラウマを抱えてひきこもりになったじんたんの元に、めんまが幽霊となって現れる。彼女に「お願いを叶えて欲しい」と頼まれたじんたんは、「超平和バスターズ」のメンバーに会いに行くが、めんまの姿が見えるのはじんたんだけで、状況を理解し、協力してくれるはずもなかった。それでもなんとか全員を集めるが、幼かった頃のようにうまくいかない。めんまが亡くなった「あの日」に縛られていたのは、じんたんだけではなかったのだ。願いを叶えようと奔走する中で、彼らはずっと隠していた思いをぶつけ合い、友情を取り戻していく。</p>
 		</div><!--box1-->
-		<div class="ttt">NEWS</div>
 		<div id="box2">
-			<div class="n_box">
-				<img src="img/sample.jpg">
-				テキストが入ります
-			</div>
-			<div class="n_box">
-				<img src="img/sample.jpg">
-				テキストが入ります
-			</div>
-			<div class="n_box">
-				<img src="img/sample.jpg">
-				テキストが入ります
+			<div class="ttt">NEWS</div>
+			<div class="left">
+				<div class="n_box">
+					<img src="img/sample.jpg">
+					テキストが入ります
 				</div>
-			<div class="n_box">
-				<img src="img/sample.jpg">
-				テキストが入ります
+				<div class="n_box">
+					<img src="img/sample.jpg">
+					テキストが入ります
+				</div>
+				<div class="n_box">
+					<img src="img/sample.jpg">
+					テキストが入ります
+				</div>
 			</div>
-			<div class="n_box">
-				<img src="img/sample.jpg">
-				テキストが入ります
-			</div>
-			<div class="n_box">
-				<img src="img/sample.jpg">
-				テキストが入ります
+
+			<div class="right">
+				<div class="n_box">
+					<img src="img/sample.jpg">
+					テキストが入ります
+				</div>
+				<div  class="n_box">
+					<img src="img/sample.jpg">
+					テキストが入ります
+				</div>
+				<div class="n_box">
+					<img src="img/sample.jpg">
+					テキストが入ります
+				</div>
 			</div>
 		</div>
 	</div><!--lefttColumn-->
