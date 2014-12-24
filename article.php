f874c6f9936e9263d6fbd422cbecc14646b3040d
mil-cre-mar <tcnk227@gmail.com>
2014-12-24 21:22:01 +0900

    すけじゅーる
    あ

--- article.php
+++ article.php
@@ -12,13 +12,13 @@
 <title>HAL Cinema</title>
 <?php
     include 'head.php';
-    
-    $todayYear = date("Y");
+	
+	$todayYear = date("Y");
     $todayMonth = date("n");
     $todayDay = date("d");
     $todayWeek = date("w");
-    $today = $todayYear.$todayMonth.$todayDay;
-    
+    $todayDate = $todayYear.$todayMonth.$todayDay;
+	
     $week = array("日","月","火","水","木","金","土");
     
     //DB接続
@@ -30,11 +30,14 @@
     mysql_select_db("halcinema",$con);
     
     //SQL作成
-    $sql = "SELECT movie_id,title,image FROM movie WHERE '$today' >= opening_day && '$today' <= end_day";
-    $sql2 = "SELECT count(*) FROM movie WHERE '$today' >= opening_day && '$today' <= end_day";
+    $sql = "SELECT movie_id,title,image FROM movie WHERE '$todayDate' >= opening_day && '$todayDate' <= end_day";
+    $sql2 = "SELECT s.movie_id , s.theater_id , s.start_time FROM screening_time AS s JOIN movie AS m ON s.movie_id = m.movie_id WHERE '$todayDate' = s.date";
+	$sql3 = "SELECT count(*) FROM screening_time AS s JOIN movie AS m ON s.movie_id = m.movie_id WHERE '$todayDate' = s.date";
+	
     //SQL実行
     $res = mysql_query($sql,$con);
-    $count = mysql_query($sql2,$con);
+    $res2 = mysql_query($sql2,$con);
+	$res3 = mysql_query($sql3,$con);
 ?>
 
 <div id="contentsWrap">
@@ -43,47 +46,66 @@
     	<div id="day">
 
             <ul class="days">
-        		<li>1/2</li>
-        		<li>1/2</li>
-                <li>1/2</li>
-                <li>1/2</li>
-                <li>1/2</li>
-                <li>1/2</li>
-                <li>1/2</li>
-            </ul>
+<?php
+	for($i = 0; $i < 7; $i++){
+		$targetDateStamp = mktime(0,0,0,$todayMonth,$todayDay + $i,$todayYear);
+		$targetDay = date("d",$targetDateStamp);
+		$targetMonth = date("n",$targetDateStamp);
+		$targetYear = date("Y",$targetDateStamp);
+		$targetWeek = date("w",$targetDateStamp);
 
+    	echo "<li id='".$i."' class='week'>".$targetMonth."/".$targetDay."（".$week[$targetWeek]."）</li>";
+	}
+?>
+            </ul>
     	</div>
 
+<?php
+	if($row3 = mysql_fetch_array($res3)){
+		if($row3[0] != 0){
+			while($row = mysql_fetch_array($res)){
+?>
     	<div class="sttt">
-    	       作品名
+    	       <?= $row[1] ?>
     	</div>
         <div class="screenimg">
             <div class="iimmgg">
-                画像
+                <?= $row[2] ?>
             </div>
         </div>
-    	<ul class="lst">
-            <li class="screen">シアター1</li>
-            <li class="movietime">13:00～</li>
-            <li class="movietime">15:00～</li>
-            <li class="movietime">17:00～</li>
+<?php
+				mysql_data_seek($res2,0);
+				while($row2 = mysql_fetch_array($res2)){
+					if($row[0] == $row2[0]){
+						if($row2[1] == "T01"){
+						$theater = "シアター01";
+						}else if($row2[1] == "T02"){
+							$theater = "シアター02";
+						}else if($row2[1] == "T03"){
+							$theater = "シアター03";
+						}else if($row2[1] == "T04"){
+							$theater = "シアター04";
+						}else if($row2[1] == "T05"){
+							$theater = "シアター05";
+						}else if($row2[1] == "T06"){
+							$theater = "シアター06";
+						}else if($row2[1] == "T07"){
+							$theater = "シアター07";
+						}else if($row2[1] == "T08"){
+							$theater = "シアター08";
+						}			
+?>
+		<ul class="lst">
+            <li class="screen"><?= $row2[2] ?>(<?= $theater ?>)</li>
+            <!-- <li class="movietime"></li> -->
         </ul>
-        <ul class="lst">
-            <li class="screen">シアター3</li>
-            <li class="movietime">13:00～</li>
-            <li class="movietime">15:00～</li>
-            <li class="movietime">17:00～</li>
-            <li class="movietime">17:00～</li>
-            <li class="movietime">17:00～</li>
-        </ul>
-
-        <ul class="lst">
-            <li class="screen">シアター5</li>
-            <li class="movietime">13:00～</li>
-            <li class="movietime">15:00～</li>
-            <li class="movietime">17:00～</li>
-        </ul>
-
+<?php
+					}
+				}
+			}
+		}
+	}
+?>
     </main>
 
     <div id="divide">
--- js/article.js
+++ js/article.js
@@ -1,13 +1,3 @@
-
 $(function(){
-
-        $("#back").click(function(){
-            $(".days").animate({'left':'+=110px'},500);
-        });
-
-        $("#next").click(function(){
-            $(".days").animate({'left':'-=110px'},500);
-        });
-
-        
-    });
+	
+});
