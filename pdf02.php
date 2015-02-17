b4b0e506ac5d61ead21fbf2cbe7c04695d52bd65
asano miki <silmeria.c@gmail.com>
2015-02-17 10:13:06 +0900

    pdf

--- pdf02.php
+++ pdf02.php
@@ -1,4 +1,5 @@
 <?php
+	$movieId = $_POST["movieSelect"];
 	require('fpdf/mbfpdf.php');
 	$pdf = new MbfPDF();
 	$pdf -> addpage();
@@ -8,15 +9,13 @@
 	$con = mysql_connect("localhost","root","");
 	mysql_set_charset("utf8");
 	mysql_select_db("halcinema",$con);
-	$sql = "SELECT count(*) AS kensu,title,reserved_time FROM reserve AS a JOIN movie AS b ON a.movie_id = b.movie_id WHERE ok = 'ok' GROUP BY DATE_FORMAT(a.reserved_time, '%Y%m'),b.movie_id ORDER BY reserved_time DESC";
+	$sql = "SELECT count(*) AS kensu FROM reserve AS a JOIN user AS b ON a.user_id = b.user_id WHERE ok = 'ok' GROUP BY DATE_FORMAT(b.birth_date, '%Y%'),b.sex";
 	$res = mysql_query($sql,$con);
 	while($row = mysql_fetch_array($res)){
 		$a = mb_convert_encoding($row["kensu"],"SJIS","UTF-8");
-		$b = mb_convert_encoding($row["title"],"SJIS","UTF-8");
-		$time = date('m',strtotime($row["reserved_time"]));
 				$y = $pdf -> getY();
 				$pdf -> setXY(10,$y+10);
-				$pdf -> write(10,$time."Œ:".$b.":".$a."l");
+				$pdf -> write(10,$a);
 	}
 	mysql_close($con);
 	$pdf -> output();
