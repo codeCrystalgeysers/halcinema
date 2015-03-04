9e871bd5d04c77eff8c090e676c56af98e35b1af
mil-cre-mar <tcnk227@gmail.com>
2015-03-04 13:44:18 +0900

    すけじゅーる
    あ

--- article.php
+++ article.php
@@ -14,10 +14,22 @@
     include 'head.php';
 	
 	$todayYear = date("Y");
-    $todayMonth = date("n");
+    $todayMonth = date("m");
     $todayDay = date("d");
     $todayWeek = date("w");
-    $todayDate = $todayYear.$todayMonth.$todayDay;
+	$todayDate = $todayYear.$todayMonth.$todayDay;
+	
+	if(isset($_GET["num"])){
+		$num = $_GET["num"];
+		
+		if($num > 6){
+			header("location:article.php");
+			exit;
+		}
+		
+		$todayDay2 = sprintf("%02d", $todayDay + $num);
+		$todayDate = $todayYear.$todayMonth.$todayDay2;
+	}
 	
     $week = array("日","月","火","水","木","金","土");
     
@@ -30,14 +42,20 @@
     mysql_select_db("halcinema",$con);
     
     //SQL作成
-    $sql = "SELECT movie_id,title,image FROM movie WHERE '$todayDate' >= opening_day && '$todayDate' <= end_day";
-    $sql2 = "SELECT s.movie_id , s.theater_id , s.start_time FROM screening_time AS s JOIN movie AS m ON s.movie_id = m.movie_id WHERE '$todayDate' = s.date";
-	$sql3 = "SELECT count(*) FROM screening_time AS s JOIN movie AS m ON s.movie_id = m.movie_id WHERE '$todayDate' = s.date";
+    //$sql = "SELECT movie_id,title,image FROM movie WHERE $todayDate >= opening_day && $todayDate <= end_day";
+    //$sql2 = "SELECT s.movie_id , s.theater_id , s.start_time FROM screening_time AS s JOIN movie AS m ON s.movie_id = m.movie_id WHERE $todayDate = s.date";
+	//$sql3 = "SELECT count(*) FROM screening_time AS s JOIN movie AS m ON s.movie_id = m.movie_id WHERE $todayDate = s.date";
+	//$sql = "SELECT m.title,m.image,s.start_time,s.theater_id FROM movie AS m JOIN screening_time AS s ON m.movie_id = s.movie_id WHERE m.movie_id IN (SELECT s.movie_id FROM screening_time AS s WHERE s.date = '$todayDate')";
+	
+	//$sql = "SELECT m.title,m.image,s.start_time,s.theater_id FROM movie AS m JOIN screening_time AS s ON m.movie_id = s.movie_id WHERE s.date = '$todayDate'";
+	
+	$sql = "SELECT m.movie_id,m.title,m.image FROM movie AS m WHERE m.movie_id IN (SELECT s.movie_id FROM screening_time AS s WHERE s.date = '$todayDate')";
+	$sql2 = "SELECT m.movie_id,s.start_time,t.theater_name FROM screening_time AS s JOIN movie AS m ON s.movie_id = m.movie_id JOIN theater AS t ON s.theater_id = t.theater_id WHERE s.date = '$todayDate'";
 	
     //SQL実行
     $res = mysql_query($sql,$con);
     $res2 = mysql_query($sql2,$con);
-	$res3 = mysql_query($sql3,$con);
+	//$res3 = mysql_query($sql3,$con);
 ?>
 
 <div id="contentsWrap">
@@ -50,23 +68,38 @@
 	for($i = 0; $i < 7; $i++){
 		$targetDateStamp = mktime(0,0,0,$todayMonth,$todayDay + $i,$todayYear);
 		$targetDay = date("d",$targetDateStamp);
-		$targetMonth = date("n",$targetDateStamp);
+		$targetMonth = date("m",$targetDateStamp);
 		$targetYear = date("Y",$targetDateStamp);
 		$targetWeek = date("w",$targetDateStamp);
-
-    	echo "<li id='".$i."' class='week'>".$targetMonth."/".$targetDay."（".$week[$targetWeek]."）</li>";
+		
+		if($targetDay < 10){
+			$targetDay = ltrim($targetDay,"0");
+		}
+		
+		if(isset($num)){
+			if($i == $num){
+				echo "<li id='".$i."' class='week todayBox'>".$targetMonth."/".$targetDay."<br/>（".$week[$targetWeek]."）</li>";
+			}else{
+    			echo "<li id='".$i."' class='week'>".$targetMonth."/".$targetDay."<br/>（".$week[$targetWeek]."）</li>";
+			}
+		}else{
+			if($i == 0){
+				echo "<li id='".$i."' class='week todayBox'>".$targetMonth."/".$targetDay."<br/>（".$week[$targetWeek]."）</li>";
+			}else{
+				echo "<li id='".$i."' class='week'>".$targetMonth."/".$targetDay."<br/>（".$week[$targetWeek]."）</li>";
+			}
+		}
 	}
 ?>
             </ul>
     	</div>
-
 <?php
-	if($row3 = mysql_fetch_array($res3)){
-		if($row3[0] != 0){
-			while($row = mysql_fetch_array($res)){
+	$cnt = 0;
+	while($row = mysql_fetch_array($res)){
+		$cnt++;
 ?>
-    	<div class="sttt">
-    	       <?= $row[1] ?>
+  		<div class="sttt">
+			<?= $row[1] ?>
     	</div>
         <div class="screenimg">
             <div class="iimmgg">
@@ -74,36 +107,21 @@
             </div>
         </div>
 <?php
-				mysql_data_seek($res2,0);
-				while($row2 = mysql_fetch_array($res2)){
-					if($row[0] == $row2[0]){
-						if($row2[1] == "T01"){
-						$theater = "シアター01";
-						}else if($row2[1] == "T02"){
-							$theater = "シアター02";
-						}else if($row2[1] == "T03"){
-							$theater = "シアター03";
-						}else if($row2[1] == "T04"){
-							$theater = "シアター04";
-						}else if($row2[1] == "T05"){
-							$theater = "シアター05";
-						}else if($row2[1] == "T06"){
-							$theater = "シアター06";
-						}else if($row2[1] == "T07"){
-							$theater = "シアター07";
-						}else if($row2[1] == "T08"){
-							$theater = "シアター08";
-						}			
+		mysql_data_seek($res2,0);
+		while($row2 = mysql_fetch_array($res2)){
+			if($row[0] == $row2[0]){
 ?>
-		<ul class="lst">
-            <li class="screen"><?= $row2[2] ?>(<?= $theater ?>)</li>
-            <!-- <li class="movietime"></li> -->
-        </ul>
+			<ul class="lst">
+         	   <li class="screen"><?= $row2[1] ?>(<?= $row2[2] ?>)</li>
+         	   <!-- <li class="movietime"></li> -->
+        	</ul>
 <?php
-					}
-				}
 			}
 		}
+	}
+	
+	if($cnt == 0){
+		echo "この日に公開する映画はありません。";
 	}
 ?>
     </main>
@@ -117,7 +135,6 @@
         <div id="links"><!-- 広告 --></div>
     </div>
 </div>
-
 <?php
     include 'foot.php';
 ?>
--- js/article.js
+++ js/article.js
@@ -1,3 +1,10 @@
 $(function(){
+	var index = 0;
 	
+	$(".week").on("click",function(){
+		var index = $("li.week").index(this);
+		location.href = "article.php?num=" + index;
+	});
+	
+	$(".todayBox").css("background-color","gold");
 });
